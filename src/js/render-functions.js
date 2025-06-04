462ac5da3aaa19c6977a10b83a2dcbc65f6856c4
Ihor222 <miskivigor10@gmail.com>
2025-06-04 15:41:35 +0300

    first

---
+++ src/js/render-functions.js
@@ -0,0 +1,43 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images.map(image => {
+    return `
+      <li class="gallery-item">
+        <a href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}" />
+        </a>
+        <div class="info">
+          <p>👍 ${image.likes}</p>
+          <p>👀 ${image.views}</p>
+          <p>💬 ${image.comments}</p>
+          <p>⬇️ ${image.downloads}</p>
+        </div>
+      </li>
+    `;
+  }).join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryContainer.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.add('visible');
+}
+
+export function hideLoader() {
+  loader.classList.remove('visible');
+}
